89dcf91d9f0103a63efb0f66fedff599c69807e1
Dan <support@stackmetric.io>
2022-03-22 16:10:47 -0500

    Added pages app

--- src/languna/settings.py
+++ src/languna/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'rmodels.apps.RmodelsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ src/rmodels/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def createmodel(request):
+    pass
+    # if request.method == 'GET':
+    #     return render(request, "{% url 'createmodel' %}", {'form': RmodelForm()})
+    # else:
+    #     form = RmodelForm(request.POST)
+    #     newmodel = form.save(commit=False)
+    #     newmodel.user = request.user
+    #     newmodel.save()
+
+
