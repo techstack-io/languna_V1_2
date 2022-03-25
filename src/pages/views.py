7dc494a55006139ce3d351a3c3646beb6368fbb4
support@oliviamcreativelabs.com Collins <support@oliviamcreativelabs.com>
2022-03-25 06:27:39 -0500

    Added summary function

--- src/pages/views.py
+++ src/pages/views.py
@@ -23,6 +23,53 @@ def models(request):
 def count(request):
 
     fulltext = request.GET['fulltext']
+
+    summary = nltk.download('punkt')
+    nltk.download('stopwords')
+    # Raw Text
+    text = re.sub(r'\[[0-9]*\]', ' ', fulltext)
+    text = re.sub(r'\s+', ' ', fulltext)
+    # Clean Text
+    clean_text = text.lower()
+    clean_text = re.sub(r'\W', ' ', clean_text)
+    clean_text = re.sub(r'\d', ' ', clean_text)
+    clean_text = re.sub(r'\s+', ' ', clean_text)
+    stopwords = nltk.corpus.stopwords.words('english')
+    word_frequency = nltk.FreqDist(nltk.word_tokenize
+                                    (clean_text))
+    # Word Dictionary
+    word2count = {}
+    for word in nltk.word_tokenize(clean_text):
+        if word not in stopwords:
+            if word not in word2count.keys():
+                word2count[word] = 1
+            else:
+                word2count[word] += 1
+
+    highest_frequency = max(word2count.values())
+    highest_frequency
+
+    # Weighted Words
+    for word in word2count.keys():
+        word2count[word] = (word2count[word] / highest_frequency)
+
+    # Tokenize sentences
+    sentences = nltk.sent_tokenize(text)
+
+    # Sentence Dictionary
+    sent2score = {}
+    for sentence in sentences:
+        for word in nltk.word_tokenize(sentence.lower()):
+            if word in word2count.keys():
+                if len(sentence.split(' ')) < 25:
+                    if sentence not in sent2score.keys():
+                        sent2score[sentence] = word2count[word]
+                    else:
+                        sent2score[sentence] += word2count[word]
+
+    best_sentences = heapq.nlargest(10, sent2score, key=sent2score.get)
+    summary = ' '.join(best_sentences)
+    summary
     # wordlist = fulltext.split()
     # return render(request, 'pages/count.html', {'fulltext':fulltext, 'count':len(wordlist)})
     ease = textstat.flesch_reading_ease(fulltext)
@@ -37,24 +84,25 @@ def count(request):
         return render(request, 'pages/count.html', {'fulltext':fulltext, 'warning':warning})
     elif 0 <= ease <= 10:
         desc1 = "this text is extremely difficult to read, and is considered a scholarly article. View the complete readability table under 'Readability Models'."
+        return render(request, 'pages/count.html', {'fulltext':fulltext, 'desc2':desc2, 'ease':ease, 'rtime':rtime, 'summary':summary})
     elif 10 <= ease <= 30:
         desc2 = "this text is very difficult to read, and best understood by college students. View the complete readability table under 'Readability Models'."
-        return render(request, 'pages/count.html', {'fulltext':fulltext, 'desc2':desc2, 'ease':ease, 'rtime':rtime})
+        return render(request, 'pages/count.html', {'fulltext':fulltext, 'desc2':desc2, 'ease':ease, 'rtime':rtime, 'summary':summary})
     elif 30 <= ease <= 50:
         desc3 = "this text is difficult to read, and best understood by university graduates. View the complete readability table under Readability Models."
-        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc3':desc3, 'ease':ease, 'rtime':rtime})
+        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc3':desc3, 'ease':ease, 'rtime':rtime, 'summary':summary})
     elif 50 <= ease <= 60:
         desc4 = "this text is fairly difficult to read. View the complete readability table under 'Readability Models'."
-        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc4':desc4, 'ease':ease, 'rtime':rtime})
+        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc4':desc4, 'ease':ease, 'rtime':rtime, 'summary':summary})
     elif 60 <= ease <= 70:
         desc6 = "this text is easily understood by 13 to 15 year old students. If you're a content writer, this is your sweet spot. View the complete readability table under 'Readability Models'."
-        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc6':desc6, 'ease':ease, 'rtime':rtime})
+        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc6':desc6, 'ease':ease, 'rtime':rtime, 'summary':summary})
     elif 70 <= ease <= 80:
         desc7 = "this text is easily understood by 13 to 15 year old students. If you're a content writer, this is your sweet spot. View the complete readability table under 'Readability Models'."
-        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc7':desc7, 'ease':ease, 'rtime':rtime})
+        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc7':desc7, 'ease':ease, 'rtime':rtime, 'summary':summary})
     elif 80 <= ease <= 100:
         desc8 = "this text is very easily understood by 10 to 12 year old students, or a 5th grade reading level."
-        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc8':desc8, 'ease':ease, 'rtime':rtime})
+        return render(request, 'pages/count.html', {'fulltext':fulltext,'desc8':desc8, 'ease':ease, 'rtime':rtime, 'summary':summary})
 
 def summarize(request):
     # if (request.GET.get('summarize')):
@@ -81,4 +129,26 @@ def summarize(request):
                     word2count[word] += 1
 
         highest_frequency = max(word2count.values())
-        return render(request, 'pages/count.html', {'highest_frequency':highest_frequency})
+        highest_frequency
+
+        # Weighted Words
+        for word in word2count.keys():
+            word2count[word] = (word2count[word] / highest_frequency)
+
+        # Tokenize sentences
+        sentences = nltk.sent_tokenize(text)
+
+        # Sentence Dictionary
+        sent2score = {}
+        for sentence in sentences:
+            for word in nltk.word_tokenize(sentence.lower()):
+                if word in word2count.keys():
+                    if len(sentence.split(' ')) < 25:
+                        if sentence not in sent2score.keys():
+                            sent2score[sentence] = word2count[word]
+                        else:
+                            sent2score[sentence] += word2count[word]
+
+        best_sentences = heapq.nlargest(10, sent2score, key=sent2score.get)
+        summary = ' '.join(best_sentences)
+        summary
